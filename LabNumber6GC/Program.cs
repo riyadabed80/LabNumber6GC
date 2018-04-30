8e6f9ea618134839ae2ba2f465fd297bdd1fee2a
Riyad <rayabed@gmail.com>
2018-04-30 06:57:32 -0400

    First Commit

---
+++ LabNumber6GC/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber6GC
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            bool UserContinue = true;
+            while (UserContinue == true)
+            {
+                Console.WriteLine("Welcome to the Grand Circus Casino! Roll the dice?");
+                string start = Console.ReadLine();
+                if (start.ToLower() == "no" || start.ToLower() == "n")
+                {
+                    UserContinue = false;
+                }
+
+
+                Console.WriteLine("Enter number of sides of your dice ");
+                int sides = (int.Parse(Console.ReadLine()));
+
+                Console.WriteLine((RandomDie(sides)));
+                Console.WriteLine((RandomDie(sides)));
+
+                Console.WriteLine("Do you want to play gain? y/n?");
+                string play = Console.ReadLine();
+
+                if (play.ToLower() == "no" || play.ToLower() == "n")
+                {
+                    UserContinue = false;
+                }
+
+                }
+
+
+            }
+
+
+        public static int RandomDie(int max)
+        {
+            //int roll;
+            Random random = new Random();
+
+            int dice = random.Next(1, max + 1);
+            //return dice;
+
+            return dice;
+        }
+    }
+}
